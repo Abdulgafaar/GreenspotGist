f8b80e588a8f20971db8e74d2cf27c05d3706322
ABDULGAFAAR ABIOYE <32104160+Abdulgafaar@users.noreply.github.com>
2018-07-15 14:46:14 +0100

    An updated version 1.0 of the project

---
+++ action.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	/* This will be executed after the DOM has been completely loaded. */
+	
+	$('nav a,footer a.up').click(function(e){
+										  
+		// once a link is been clicked, scroll the page to the link's hash target:
+		
+		$.scrollTo( this.hash || 0, 1500);
+		e.preventDefault();
+	});
+
+});
